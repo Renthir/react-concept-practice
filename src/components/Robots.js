871069e3b7f0913462d31ba39b9984fc71586fa6
Seth Reid <reid.seth.96@gmail.com>
2023-08-11 13:46:01 -0600

    will this work?

--- src/components/Robots.js
+++ src/components/Robots.js
@@ -1,6 +1,10 @@
 function Robots() {
     return (
-        <h2>What is a robot, and what is robot.txt doing in this small practice module?</h2>
+        <div>
+            <h2>What is a robot, and what is robot.txt doing in this small practice module?</h2>
+            <h3>I'm not sure and I'm not sure I care to find out</h3>
+        </div>
+
     )
 }
 
